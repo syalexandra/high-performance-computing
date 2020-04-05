752a649e4526a325208aa81fb74f30da832d9c3f
syalexandra <sunyuealexandra@gmail.com>
2020-04-05 15:43:53 -0400

    save

--- homework03/omp-scan.cpp
+++ homework03/omp-scan.cpp
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <omp.h>
 
+#define nthreads 8
 // Scan A array and write result into prefix_sum array;
 // use long data type to avoid overflow
 void scan_seq(long* prefix_sum, const long* A, long n) {
@@ -11,10 +12,41 @@ void scan_seq(long* prefix_sum, const long* A, long n) {
   for (long i = 1; i < n; i++) {
     prefix_sum[i] = prefix_sum[i-1] + A[i-1];
   }
+  
 }
 
 void scan_omp(long* prefix_sum, const long* A, long n) {
   // TODO: implement multi-threaded OpenMP scan
+    if (n == 0) return;
+    prefix_sum[0] = 0;
+    omp_set_num_threads(nthreads);
+    int threadLen=n/nthreads;
+    long*sumList= (long*) malloc((nthreads+1) * sizeof(long));
+       
+    #pragma omp parallel for
+    for(long i=0;i<nthreads;i++)
+    {
+        long index;
+        prefix_sum[i*threadLen]=0;
+        for(long j =1;j<threadLen;j++){
+            index=i*threadLen+j;
+            prefix_sum[index]=prefix_sum[index-1]+A[index-1];
+        }
+        sumList[i+1]=prefix_sum[index]+A[index];
+    }
+    
+    for(long i=1;i<nthreads;i++){
+        sumList[i]=sumList[i]+sumList[i-1];
+    }
+    
+    
+    #pragma omp parallel for
+    for(long i=0;i<nthreads;i++){
+        for(long j=0;j<threadLen;j++){
+            long index=i*threadLen+j;
+            prefix_sum[index]+=sumList[i];
+        }
+    }
 }
 
 int main() {
@@ -22,7 +54,7 @@ int main() {
   long* A = (long*) malloc(N * sizeof(long));
   long* B0 = (long*) malloc(N * sizeof(long));
   long* B1 = (long*) malloc(N * sizeof(long));
-  for (long i = 0; i < N; i++) A[i] = rand();
+    for (long i = 0; i < N; i++) A[i] = i;
 
   double tt = omp_get_wtime();
   scan_seq(B0, A, N);

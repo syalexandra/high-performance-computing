fb9a0f26eecd43e42f57350d0d502c79bf4812b5
syalexandra <sunyuealexandra@gmail.com>
2020-04-19 19:08:12 -0400

    save

--- hw4/matrix_vector.cu
+++ hw4/matrix_vector.cu
@@ -11,49 +11,48 @@
 #include <string>
 
 
-void vec_inner_product(double* c, const double* a, const double* b){
-    double temp=0;
-    #pragma omp parallel for shared(temp)
+#define BLOCK_SIZE (1UL<<10)
+#define GRID_SIZE (1UL<<10)
+
+void vec_mat_product(double* c, const double* a, const double* b,long N,long M){
+    
+    #pragma omp parallel for schedule(static)
     for (long i = 0; i < N; i++) {
-        temp += a[i] * b[i];
+    
+        double temp=0;
+        for(long j = 0; j < M; j++){
+            temp+=a[i*M+j]*b[j];
+        }
+        
+        c[i]=temp;
     }
-    
-    *c=temp;
-    
 }
 
 
 
 __global__
-void vec_inner_product_kernel(double* c,const double* a, const double* b){
+void vec_mat_product_kernel(double* c,const double* a, const double* b,long N,long M){
 
-    __shared__ float chache[BLOCK_SIZE] ;
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    int chacheindex = threadIdx.x;
-    double temp;
+
     
-    while ( tid < N )
-    {
-         temp += a[tid] * b[tid] ;
-         tid += blockDim.x * gridDim.x ;
+    __shared__ double temp[M];
+    
+    int col=threadIdx.x+blockDim.x*blockIdx.x;
+    int row=blockIdx.x*gridDim.x+blockIdx.y;
+    
+    if(col<M){
+        temp[col]=a[row*M+col]*b[col];
     }
     
-    chache[chacheindex] = temp ;
-
-    __synchthreads ();
-    int i  = blockDim.x / 2;
-    while ( i!=0 )
-     {
-        if ( chacheindex < i )
-            chache[chacheindex] += chache [chacheindex + i] ;
-        
-        __synchthreads();
-        i/=2 ;
-     }
-     
-     if ( chacheindex == 0 )c[blockIdx.x] = chache [0] ;
-
+    __syncthreads();
     
+    if(0==threadIdx.x){
+        int summ=0;
+        for(int i=0;i<M;i++){
+            summ+=temp[i];
+        }
+        c[row]=summ;
+    }
 }
 
 
@@ -69,8 +68,8 @@ void Check_CUDA_Error(const char *message){
 
 int main() {
 
-    #define N (1UL<<15)
-    #define M (1UL<<10)
+    long N= (1UL<<20)
+    long M= (1UL<<10)
     
     double *x, *y, *z;
     
@@ -78,6 +77,7 @@ int main() {
     cudaMallocManaged(&y, M * sizeof(double));
     cudaMallocManaged(&z, N * sizeof(double));
     
+    double* z_ref = (double*) malloc(N * sizeof(double));
     
     #pragma omp parallel for schedule(static)
     for (long i = 0; i < N*M; i++)
@@ -92,26 +92,23 @@ int main() {
     }
     
     
-    
     double tt = omp_get_wtime();
-    vec_inner_product(z_ref,x, y);
+    vec_mat_product(z_ref, x, y, N);
     printf("CPU Bandwidth = %f GB/s\n", 3*N*sizeof(double) / (omp_get_wtime()-tt)/1e9);
+    
     
 
     tt = omp_get_wtime();
-    vec_inner_product_kernel<<<N/BLOCK_SIZE+1,BLOCK_SIZE>>>(z,x,y);
+    dim3 dimGrid(N/GRID_SIZE,GRID_SIZE);
+    dim3 dimBlock(M/BLOCK_SIZE,BLOCK_SIZE);
+    
+    vec_mat_product_kernel<<<dimGrid,dimBlock>>>(z,x,y,N,M);
     cudaDeviceSynchronize();
     
-    float sum = 0;
-    for ( int i = 0 ; i<BLOCK_SIZE ; i++ )
-        sum+=s[i];
     printf("GPU Bandwidth = %f GB/s\n", 3*N*sizeof(double) / (omp_get_wtime()-tt)/1e9);
     
-    
-    double err = 0;
-    err=s_ref-sum;
-    printf("Error = %f %f %f\n", err,s_ref,sum);
-    
+    double err=0;
+    for (long i = 0; i < N; i++) err += fabs(z[i]-z_ref[i]);
     
     
 }

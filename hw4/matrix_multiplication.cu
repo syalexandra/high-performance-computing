51a185198dd91361ae1c4d9cf2f16d4e0f3038fe
syalexandra <sunyuealexandra@gmail.com>
2020-04-19 15:53:40 -0400

    save

--- hw4/matrix_multiplication.cu
+++ hw4/matrix_multiplication.cu
@@ -110,7 +110,7 @@ int main() {
     
     long N_work = 1;
     for (long i = (N+BLOCK_SIZE-1)/(BLOCK_SIZE); i > 1; i = (i+BLOCK_SIZE-1)/(BLOCK_SIZE)){N_work += i;}
-    printf("%d",N_work);
+    //printf("%ld",N_work);
     /*
     cudaMalloc(&s, N_work*sizeof(double));
     

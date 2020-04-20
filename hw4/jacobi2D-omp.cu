e28aa8b4921dce90e38327be68f7384f73135252
syalexandra <sunyuealexandra@gmail.com>
2020-04-20 10:53:18 -0400

    save

--- hw4/jacobi2D-omp.cu
+++ hw4/jacobi2D-omp.cu
@@ -132,7 +132,7 @@ int main(int argc, char ** argv) {
     dim3 GridDim((N+2)/BLOCK_SIZE,(N+2)/BLOCK_SIZE);
     dim3 BlockDim(BLOCK_SIZE, BLOCK_SIZE);
     
-    for(int i=0;i<1000;i++){
+    for(int i=0;i<100;i++){
         if(i%2==0){
             jacobiUpdate<<<GridDim,BlockDim>>>(x_d,x_next_d,f_d);
         }
@@ -148,8 +148,8 @@ int main(int argc, char ** argv) {
     cudaMemcpy(x_next, x_next_d, (N+2)*(N+2)* sizeof(double), cudaMemcpyDeviceToHost);
     double error=0.0;
     
-    for(int i=1;i<(N+2)*(N+2);i++){
-        //printf("%f,%f\n",x[i],x_next[i]);
+    for(int i=(N+2)*(N+2)/2;i<(N+2)*(N+2);i++){
+        printf("%f,%f\n",x[i],x_next[i]);
         error+=(x[i]-x_next[i])*(x[i]-x_next[i]);
     }
     

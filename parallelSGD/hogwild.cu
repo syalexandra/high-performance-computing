52bcdda8f80e3d3af7b5ae3418f3bab99dab70c5
syalexandra <sunyuealexandra@gmail.com>
2020-04-26 13:29:37 -0400

    Save

--- parallelSGD/hogwild.cu
+++ parallelSGD/hogwild.cu
@@ -21,7 +21,7 @@
 using namespace std;
 typedef unsigned char uchar;
 
-__device__ double getOneGradient(double* weight,int index,const double*trainingData,const uchar* trainingLabel,double eta,int n_data,int n_weights,int n_labels){
+__global__ double getOneGradient(double* weight,int index,const double*trainingData,const uchar* trainingLabel,double eta,int n_data,int n_weights,int n_labels){
     
     printf("%d %d %d",n_data,n_weights,n_labels);
     

99a55bd2e99899b0f9de90dde0e7c3dbfae216d8
syalexandra <sunyuealexandra@gmail.com>
2020-04-27 20:08:10 -0400

    save

--- parallelSGD/hogwild.cu
+++ parallelSGD/hogwild.cu
@@ -89,7 +89,7 @@ __global__ void updateWeightKernel(double* weight,const double* trainingData,con
             //printf("%d ",label[b]);
             for(int w=0;w<n_weights;w++){
                 data[b*n_weights+w]=trainingData[r*n_weights+w];
-                //printf("%d %d %f \n",r,w,trainingData[r*n_weights+w]);
+                printf("%d %d %f \n",r,w,trainingData[r*n_weights+w]);
             }
         }
         printf("end");

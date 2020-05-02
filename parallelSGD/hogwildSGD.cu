e6356ebcded1e70585116b08416075571edc1e53
syalexandra <sunyuealexandra@gmail.com>
2020-05-02 19:41:44 -0400

    save

--- parallelSGD/hogwildSGD.cu
+++ parallelSGD/hogwildSGD.cu
@@ -189,7 +189,7 @@ int main(int argc, const char * argv[]) {
     	if(j %(n_iterations/5) == 0 || j == n_iterations-1){
           cudaMemcpy(weight, weight_d, weight_size*sizeof(double), cudaMemcpyDeviceToHost);
 	  cudaDeviceSynchronize();
-          printf("weight[107] = %f\n", weight[107]);
+          printf("weight_d[107] = %f\n", weight_d[107]);
 	  double loss_now = getLoss(weight, tempData, tempLabel, n_images, size_image+1,10, lambda);
 	  printf("Training (log)loss: %f\t thread:%d\n",loss_now, omp_get_thread_num());
 	  psgd.testGPU(weight, testingData, testingLabels, n_images_test, size_image+1, 10);

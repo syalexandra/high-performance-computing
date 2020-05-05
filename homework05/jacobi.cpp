50c0153089aab13aeadbebf877b578edd31d4602
syalexandra <sunyuealexandra@gmail.com>
2020-05-05 11:43:05 -0400

    save

--- homework05/jacobi.cpp
+++ homework05/jacobi.cpp
@@ -145,10 +145,10 @@ int main(int argc, char * argv[]){
     lutemp = lu; lu = lunew; lunew = lutemp;
     //if (0 == (iter % 10)) {
       gres = compute_residual(lu, Nl, invhsq);
-      printf("%d rank %d\n",iter,mpirank);
-      for(int i=0;i<Nl;i++){
-          for(int j=0;j<Nl;j++){
-              printf("%f",lu[i*(Nl+2)+j]);
+      printf("iteration %d\n",iter);
+      for(int i=0;i<Nl+2;i++){
+          for(int j=0;j<Nl+2;j++){
+              printf("%d,%d,%d,%f",mpirank,i,j,lu[i*(Nl+2)+j]);
           }
           printf("\n");
       }
